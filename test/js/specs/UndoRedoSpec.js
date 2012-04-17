70c9f3841037ef2f9d458352b2314c4a9f9db06b
Serj Ryabokon <ryabokon.serhiy@gmail.com>
2012-04-17 07:22:13 +0300

    added tests for undo redo

--- test/js/specs/UndoRedoSpec.js
+++ test/js/specs/UndoRedoSpec.js
@@ -35,7 +35,7 @@ describe('undo-redo-spec', function(){
    /**
    * Tests are listed below:
    */
-   it("when no actions nothing is changed by undo/redo", function() {
+   it("when no actions nothing is changed by undo", function() {
       
       var el = $("#" + _id).get(0);
       pte.bindToElem(el);
@@ -47,5 +47,167 @@ describe('undo-redo-spec', function(){
       expect(txt).toBe(_content);
       expect(txt).not.toBe(_emptyString);
    });
+
+   it("when no actions nothing is changed by redo", function() {
+      
+      var el = $("#" + _id).get(0);
+      pte.bindToElem(el);
+      
+      var txt = pte.redo();
+      
+      var txt = pte.getText();
+      
+      expect(txt).toBe(_content);
+      expect(txt).not.toBe(_emptyString);
+   });
    
+   it("undo-redo for first symbol being typed", function() {
+      var el = $("#" + _id).get(0);
+      
+      pte.bindToElem(el);
+      
+      var akey = 65;
+      var keyDownEv = $.Event('keydown', { keyCode: akey, which: akey }) ;
+      
+      /**
+      * Event occurs for PlainTextEditor, 
+      * but internally no character is typed
+      * into an element, so it's content
+      * remains the same
+      */
+      $(document).trigger( keyDownEv );
+      
+      
+      
+      var txt = pte.undo();
+      var txt = pte.redo();
+      var changedContent = "a'est \n tres belle";
+      
+      var txt = pte.getText();
+      
+      expect(txt).toBe(changedContent);
+      expect(txt).not.toBe(_emptyString);
+   });
+   
+   it("undo for replace", function() {
+      var el = $("#" + _id).get(0);
+      
+      pte.bindToElem(el);
+      
+      pte.replace("s'", "c'");
+      
+      var txt = pte.undo();
+      
+      var changedContent = "s'est \n tres belle";
+      
+      var txt = pte.getText();
+      
+      expect(txt).toBe(changedContent);
+      expect(txt).not.toBe(_emptyString);
+   });
+
+   it("redo for replace", function() {
+      var el = $("#" + _id).get(0);
+      
+      pte.bindToElem(el);
+      
+      pte.replace("s'", "c'");
+      
+      
+      pte.undo();
+      pte.redo();
+            
+      var txt = pte.getText();
+      
+      expect(txt).toBe(_content);
+      expect(txt).not.toBe(_emptyString);
+   });
+
+   it("undo for replaceAll", function() {
+      var el = $("#" + _id).get(0);
+      
+      pte.bindToElem(el);
+      
+      pte.replaceAll("es", "ui");
+      
+      pte.undo();
+      
+      var txt = pte.getText();
+      
+      expect(txt).toBe(_content);
+      expect(txt).not.toBe(_emptyString);
+   });
+   
+   it("undo for replaceInSel", function() {
+      var el = $("#" + _id).get(0);
+      
+      pte.bindToElem(el);
+      
+      pte.setSelection(0, 5);
+      pte.replaceInSel("es", "NEW");      
+      pte.undo();
+      
+            
+      var txt = pte.getText();
+      
+      expect(txt).toBe(_content);
+      expect(txt).not.toBe(_emptyString);
+   });     
+   
+   it("redo for replaceInSel", function() {
+      var el = $("#" + _id).get(0);
+      
+      pte.bindToElem(el);
+      
+      pte.setSelection(0, 5);
+      pte.replaceInSel("es", "NEW");      
+      pte.undo();
+      pte.redo();
+            
+      var txt = pte.getText();
+      var newContent = "c'NEWt \n tres belle";;
+      expect(txt).toBe(newContent);
+      expect(txt).not.toBe(_emptyString);
+   });
+   
+   it("undo for cut", function() {
+      var el = $("#" + _id).get(0);
+      
+      pte.bindToElem(el);
+      
+      pte.replace("s'", "c'");
+      
+      
+      pte.setSelection(0, 3);
+      pte.cut();
+      pte.undo();
+      
+      
+      var txt = pte.getText();
+      
+      expect(txt).toBe(_content);
+      expect(txt).not.toBe(_emptyString);
+   });
+ 
+   it("redo for cut", function() {
+      var el = $("#" + _id).get(0);
+      
+      pte.bindToElem(el);
+      
+      pte.replace("s'", "c'");
+      
+      
+      pte.setSelection(0, 3);
+      pte.cut();
+      pte.undo();
+      pte.redo();
+      
+      
+      var txt = pte.getText();
+      var editedContent = "st \n tres belle";
+      
+      expect(txt).toBe(editedContent);
+      expect(txt).not.toBe(_emptyString);
+   });
+      
 });
